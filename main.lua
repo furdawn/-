aa8e8466739c5e64e14f49d8d9ab2e1379fa15ba
furdawn <138234425+furdawn@users.noreply.github.com>
2024-07-14 03:29:06 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -215,14 +215,14 @@ local function meowfag()
         firstTween:Play()
         firstTween.Completed:Wait()
 
-        local upTweenInfo = TweenInfo.new(0.15, Enum.EasingStyle.Linear)
+        local upTweenInfo = TweenInfo.new(0.35, Enum.EasingStyle.Linear)
         local upTween = TweenService:Create(humanoidRootPart, upTweenInfo, {
             CFrame = CFrame.new(coin.Position - Vector3.new(0, 3.4, 0))
         })
         upTween:Play()
         upTween.Completed:Wait()
 
-        local downTweenInfo = TweenInfo.new(0.15, Enum.EasingStyle.Linear)
+        local downTweenInfo = TweenInfo.new(0.25, Enum.EasingStyle.Linear)
         local downTween = TweenService:Create(humanoidRootPart, downTweenInfo, {
             CFrame = CFrame.new(coin.Position - Vector3.new(0, 6, 0))
         })
