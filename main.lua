29e78d83462a9bbbcb790c7674229a0d9cc1cbca
furdawn <138234425+furdawn@users.noreply.github.com>
2024-07-28 11:31:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,13 +22,6 @@ local function meowfag()
     inLobby.Anchored = true
     inLobby.Parent = workspace
 
-    for _, v in pairs(getconnections(game.Players.LocalPlayer.Idled)) do
-        if v["Disable"] then
-            v["Disable"](v)
-        elseif v["Disconnect"] then
-            v["Disconnect"](v)
-        end
-    end
     local VirtualUser = (game:GetService("VirtualUser"))
     Players.LocalPlayer.Idled:Connect(function()
         VirtualUser:CaptureController()
@@ -121,8 +114,70 @@ local function meowfag()
         end
     end
 
+    local function endRound()
+        flinging = false
+        local targetPlayer = nil
+        local roles = ReplicatedStorage:FindFirstChild("GetPlayerData", true):InvokeServer()
+        for i, v in pairs(roles) do
+            if v.Role == "Murderer" then
+                targetPlayer = Players:FindFirstChild(i)
+                break
+            end
+        end
+        if Players.LocalPlayer == targetPlayer then
+            resetCharacter()
+            return
+        end
+        for _, child in pairs(Players.LocalPlayer.Character:GetDescendants()) do
+            if child:IsA("BasePart") then
+                child.CustomPhysicalProperties = PhysicalProperties.new(100, 0.3, 0.5)
+            end
+        end
+
+        task.wait(1)
+
+        local poofMurderer = Instance.new("BodyAngularVelocity")
+        poofMurderer.Name = "SmirksWithMaliciousIntent"
+        poofMurderer.Parent = Players.LocalPlayer.Character.HumanoidRootPart
+        poofMurderer.AngularVelocity = Vector3.new(0, 95000, 0)
+        poofMurderer.MaxTorque = Vector3.new(0, math.huge, 0)
+        poofMurderer.P = math.huge
+        local Char = Players.LocalPlayer.Character:GetChildren()
+        for _, v in next, Char do
+            if v:IsA("BasePart") then
+                v.CanCollide = false
+                v.Massless = true
+                v.Velocity = Vector3.new(0, 0, 0)
+            end
+        end
+
+        flinging = true
+        while not Players.LocalPlayer or not Players.LocalPlayer.Character do
+            task.wait()
+        end
+        if targetPlayer and targetPlayer.Character and targetPlayer.Character.Humanoid then
+            local startTime = os.time()
+            while flinging == true and Players.LocalPlayer.Character and targetPlayer.Character do
+                if not Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") or not targetPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                    break
+                end
+                poofMurderer.AngularVelocity = Vector3.new(0, 95000, 0)
+                Players.LocalPlayer.Character.HumanoidRootPart.CFrame = targetPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(8), 0, math.rad(8))
+                task.wait(0.1)
+                poofMurderer.AngularVelocity = Vector3.new(0, 0, 0)
+                if os.time() - startTime >= 4 then
+                    break
+                end
+            end
+            flinging = false
+            resetCharacter()
+        end
+        gotoHide()
+    end
+
     local function getClosest(coinID)
         local Character = Players.LocalPlayer.Character or Players.LocalPlayer.CharacterAdded:Wait()
+        local humanoidRootPart = Character:WaitForChild("HumanoidRootPart", math.huge)
         local shortestDistance = 420420
         local closestCoin = nil
 
@@ -135,7 +190,7 @@ local function meowfag()
             if coin:IsA("BasePart") then
                 if coin:FindFirstChild("TouchInterest") then
                     if coin:GetAttribute("CoinID") == coinID or (coinID == "Meow" and coin:GetAttribute("CoinID") ~= nil) then
-                        local distance = (Character:FindFirstChild("HumanoidRootPart").Position - coin.Position).Magnitude
+                        local distance = (humanoidRootPart.Position - coin.Position).Magnitude
                         if distance < shortestDistance then
                             shortestDistance = distance
                             closestCoin = coin
@@ -215,20 +270,7 @@ local function meowfag()
 
         Noclip()
 
-        local function containerCheck()
-            local x = game.Workspace:WaitForChild("Normal", 5)
-            if not x then
-                return false
-            end
-            local y = x:FindFirstChild("CoinContainer")
-            if not y then
-                return false
-            elseif y then
-                return true
-            end
-        end
-
-        while eventAmount < 20 and coinAmount < 40 and containerCheck() do
+        while eventAmount < 20 and coinAmount < 40 do
             if not tweenInProgress then
                 local closestEither = getClosest("Meow")
                 if closestEither then
@@ -240,7 +282,7 @@ local function meowfag()
             end
         end
 
-        while eventAmount < 20 and containerCheck() do
+        while eventAmount < 20 do
             if not tweenInProgress then
                 local closestEvent = getClosest("BeachBall")
                 if closestEvent then
@@ -251,7 +293,7 @@ local function meowfag()
             end
         end
 
-        while coinAmount < 40 and containerCheck() do
+        while coinAmount < 40 do
             if not tweenInProgress then
                 local closestCoin = getClosest("Coin")
                 if closestCoin then
@@ -264,10 +306,7 @@ local function meowfag()
 
         tweenInProgress = false
 
-        if not containerCheck() then
-            gotoHide()
-            return
-        end
+        endRound()
     end
 
     gameRemote.OnClientEvent:Connect(onGameStart)
