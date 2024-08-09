f9202d1845decfe79e1e9144cfb998735422801a
furdawn <138234425+furdawn@users.noreply.github.com>
2024-08-09 02:58:31 -0500

    Update ass.lua

--- ass.lua
+++ ass.lua
@@ -11,8 +11,6 @@ local Terrain = game.Workspace.Terrain
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-print("Ewww")
-
 --- Optimization stuff :3
 game.Workspace.Gravity = 0
 Terrain.WaterWaveSize = 0
@@ -27,6 +25,38 @@ settings().Rendering.QualityLevel = "Level01"
 local function GameStart()
     print("Game started")
     LocalPlayer.Character.Animate.Disabled = true
+    local targetGui = Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target
+    local previousTarget = ""
+    local knifeFound = true
+    print("Got Data")
+
+    while knifeFound do
+        local targetText = targetGui.TargetText.Text
+        if targetText ~= previousTarget then
+            previousTarget = targetText
+            local targetPlayer = game.Players:FindFirstChild(targetText)
+            if targetPlayer and targetPlayer:FindFirstChild("Backpack") then
+                local backpack = targetPlayer.Backpack
+                local knife = backpack:FindFirstChild("Knife")
+                
+                if knife then
+                    if LocalPlayer and targetPlayer.Character and targetPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                        LocalPlayer.Character.Animate.Disabled = true
+                        local targetPosition = targetPlayer.Character.HumanoidRootPart.Position
+                        local args = {
+                            [1] = targetPosition,
+                            [2] = 0,
+                            [3] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                        }
+                        ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("ThrowKnife"):FireServer(unpack(args))
+                    end
+                else
+                    knifeFound = false
+                end
+            end
+        end
+        wait(0.1)
+    end
 end
 
 LocalPlayer.Backpack.ChildAdded:Connect(function(v)
