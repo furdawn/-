acf9f6a0ab0e73e58d1f3ddcc0fd6e461ce9bd16
furdawn <138234425+furdawn@users.noreply.github.com>
2024-08-10 16:15:05 -0500

    Update ass.lua

--- ass.lua
+++ ass.lua
@@ -1,11 +1,38 @@
 print("meow")
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Lighting = game:GetService("Lighting")
+local Terrain = game.Workspace.Terrain
 local Players = game:GetService("Players")
 
 local TargetGUI = Players.LocalPlayer.PlayerGui:FindFirstChild("ScreenGui"):FindFirstChild("UI"):FindFirstChild("Target")
 
 game.Workspace.Gravity = 0
+Terrain.WaterWaveSize = 0
+Terrain.WaterWaveSpeed = 0
+Terrain.WaterReflectance = 0
+Terrain.WaterTransparency = 0
+Lighting.Brightness = 0
+Lighting.GlobalShadows = false
+settings().Rendering.QualityLevel = 1
+
+local function DestroyMap()
+    local Map = game.Workspace:FindFirstChild("GameMap")
+    for _, v in pairs(Map:GetDescendants()) do
+        if v:IsA("BasePart") then
+            v:Destroy()
+        end
+    end
+end
+
+local function BreakVelo()
+    task.wait(0.5)
+    for _, v in ipairs(Players.LocalPlayer.Character:GetDescendants()) do
+        if v:IsA("BasePart") then
+            v.Velocity, v.RotVelocity = Vector3.zero, Vector3.zero
+        end
+    end
+end
 
 local function GotoTarget(gotoUser)
     local TargetPlayer = game.Workspace:FindFirstChild(gotoUser)
@@ -13,9 +40,10 @@ local function GotoTarget(gotoUser)
         local localHumanoid = Players.LocalPlayer.Character.HumanoidRootPart
         local targetHumanoid = TargetPlayer:FindFirstChild("HumanoidRootPart")
         if localHumanoid and targetHumanoid then
-            localHumanoid.CFrame = targetHumanoid.CFrame - CFrame.new(0, -5, -5)
+            localHumanoid.CFrame = targetHumanoid.CFrame * CFrame.new(0, -5, -5)
         end
     end
+    BreakVelo()
 end
 
 local function CheckTarget(currentUser)
@@ -26,6 +54,10 @@ local function CheckTarget(currentUser)
 end
 
 local function Assassinate()
+    DestroyMap()
+    BreakVelo()
+    Players.LocalPlayer.Character.Animate.Disabled = true
+
     print("111")
 
     while #Players.LocalPlayer:WaitForChild("Backpack"):GetChildren() == 0 do
@@ -34,17 +66,18 @@ local function Assassinate()
 
     print("222")
 
-    local knife = Players.LocalPlayer:FindFirstChildOfClass("Backpack").Knife
-    knife.Parent = Players.LocalPlayer.Character
+    local MeowGUI = Players.LocalPlayer.PlayerGui:FindFirstChild("ScreenGui"):FindFirstChild("UI"):FindFirstChild("Target")
+    local Knife = Players.LocalPlayer:FindFirstChildOfClass("Backpack").Knife
+    Knife.Parent = Players.LocalPlayer.Character
 
-    local targetUser = TargetGUI.TargetText.Text
+    local TargetUser = MeowGUI.TargetText.Text
 
     print("333")
 
-    while not CheckTarget(targetUser) do
+    while not CheckTarget(TargetUser) do
         print("444")
-        GotoTarget(targetUser)
-        local TargetPlayer = game.Workspace:FindFirstChild(targetUser)
+        GotoTarget(TargetUser)
+        local TargetPlayer = game.Workspace:FindFirstChild(TargetUser)
         if TargetPlayer and TargetPlayer:IsA("Model") and TargetPlayer:FindFirstChild("HumanoidRootPart") then
             local targetHumanoid = TargetPlayer:FindFirstChild("HumanoidRootPart")
             local args = {
