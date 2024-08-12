d95ceb163dc2a4269631fc3b1226a161f2f4488e
furdawn <138234425+furdawn@users.noreply.github.com>
2024-08-12 17:42:39 -0500

    Update uwu.lua

--- uwu.lua
+++ uwu.lua
@@ -126,7 +126,7 @@ local function Kill(targetPlayer)
 
         if localRoot and targetRoot then
             local rotation = CFrame.Angles(0, 0, math.pi * 0.5)
-            local offset = targetRoot.CFrame:vectorToWorldSpace(Vector3.new(-1.25, -2.25, 0.5))
+            local offset = CFrame.new(-1.25, -2.25, 0.5)
             local targetCFrame = targetRoot.CFrame * rotation + offset
             local tween = TweenService:Create(localRoot, TweenInfo.new((Players.LocalPlayer.Character.HumanoidRootPart - targetCFrame.Position).Magnitude / 5, Enum.EasingStyle.Linear), { CFrame = targetCFrame })
             tween:Play()
