15613fce651861b760c9640003f8e5c482817ee2
furdawn <138234425+furdawn@users.noreply.github.com>
2024-08-15 16:28:07 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,21 +6,28 @@ local Api = "https://games.roblox.com/v1/games/"
 local _place,_id = game.PlaceId, game.JobId
 local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
 
-if #game.Players:GetPlayers() <= 3 then
-    function ListServers(cursor)
-        local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-        return Http:JSONDecode(Raw)
-    end
-
-    local Next; repeat
-        local Servers = ListServers(Next)
-        for i,v in next, Servers.data do
-            if v.playing < v.maxPlayers and v.id ~= _id then
-                local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
-                if s then break end
-            end
+local function Hop()
+    if #game.Players:GetPlayers() <= 3 then
+        function ListServers(cursor)
+            local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+            return Http:JSONDecode(Raw)
         end
 
-        Next = Servers.nextPageCursor
-    until not Next
+        local Next; repeat
+            local Servers = ListServers(Next)
+            for i,v in next, Servers.data do
+                if v.playing < v.maxPlayers and v.id ~= _id then
+                    local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+                    if s then break end
+                end
+            end
+
+            Next = Servers.nextPageCursor
+        until not Next
+    end
+end
+
+while true do
+    Hop()
+    task.wait(5400)
 end
