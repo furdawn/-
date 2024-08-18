e456dc8624959a3a25af5e3df2b1a0556d5bc0c6
furdawn <138234425+furdawn@users.noreply.github.com>
2024-08-17 19:28:50 -0500

    Update meow.lua

--- meow.lua
+++ meow.lua
@@ -64,7 +64,7 @@ local function Kill(targetPlayer)
         local targetRoot = targetPlayer:FindFirstChild("HumanoidRootPart")
 
         if localRoot and targetRoot then
-            local offset = CFrame.new(-1.5, -3, 0.25)
+            local offset = CFrame.new(-1, -2.75, 0.35)
             local targetCFrame = targetRoot.CFrame * offset
             local tween = TweenService:Create(localRoot, TweenInfo.new(0, Enum.EasingStyle.Linear), { CFrame = targetCFrame })
             tween:Play()
